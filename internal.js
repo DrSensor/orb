55928015993a3c1b6c3f393485fafe3e79515e2c
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-06 20:17:40 +0700

    Support cascading orb
    Inspired from Blazor CascadingParameter attribute.
    https://docs.microsoft.com/en-us/aspnet/core/blazor/components/cascading-values-and-parameters

--- internal.js
+++ internal.js
@@ -1,21 +1,28 @@
-const { defineProperties } = Object, noop = (_) => {};
+const { defineProperties } = Object, noop = (_) => {}, { iterator } = Symbol;
 export const isFunction = ($) => typeof $ == "function";
 
 [Number, String].forEach(({ prototype: $ }) =>
   defineProperties($, {
-    [Symbol.iterator]: {
+    [iterator]: {
       value: function* () {
         let self = this, onchange = noop;
         const react = new Set();
         const orb = defineProperties(
           (...$) => {
-            if ($.length) {
-              const finalize = onchange(self = $[0]), after = [];
-              react.forEach((effect) => after.push(effect(self)));
-              while (after.length) after.shift()?.();
-              finalize?.();
+            const [$1] = $;
+            if (isFunction($1)) { // cascading transformed orb
+              const [orb$] = $[iterator].apply(self);
+              react.add((value) => orb$($1(value)));
+              return orb$;
+            } else {
+              if ($.length) {
+                const finalize = onchange(self = $1), after = [];
+                react.forEach((effect) => after.push(effect(self)));
+                while (after.length) after.shift()?.();
+                finalize?.();
+              }
+              return self;
             }
-            return self;
           },
           {
             reset: { value: () => orb(this) },
@@ -24,6 +31,11 @@ export const isFunction = ($) => typeof $ == "function";
             onchange: {
               set: (cb) => onchange = isFunction(cb) ? cb : noop,
               get: () => (cb) => isFunction(cb) && react.add(cb) && cb,
+            },
+            [iterator]: function* () { // cascading orb
+              const [orb$] = $[iterator].apply(self);
+              react.add(orb$);
+              yield orb$;
             },
           },
         );
