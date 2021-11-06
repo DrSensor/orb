dae782fb1a1d8c57295e6347309430782ec1a42a
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-06 16:12:09 +0700

    Restructure project

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
-const { defineProperties } = Object, noop = (_) => {}, id = Symbol();
-export const isFunction = ($) => typeof $ == "function";
+import { isFunction } from "./internal.js";
 
-[Number, String].forEach(({ prototype: $ }) =>
-  defineProperties($, {
-    [Symbol.iterator]: {
-      value: function* () {
-        let self = this, onchange = noop;
-        const react = [];
-        const orb = defineProperties(
-          (...$) => {
-            if ($.length) {
-              onchange(self = $[0]);
-              react.forEach((effect) => effect(self));
-            }
-            return self;
-          },
-          {
-            reset: { value: () => orb(this) },
-            value: { set: orb, get: orb },
-            unbind: {
-              value: ($) => {
-                const { length } = react;
-                isFunction($)
-                  ? react.splice(react.findIndex((cb) => cb[id] == $[id]), 1)
-                  : react.splice($ > 0 ? $ - 1 : length, 1);
-                return length;
-              },
-            },
-            onchange: {
-              set: (cb) => onchange = isFunction(cb) ? cb : noop,
-              get: () => (cb) => isFunction(cb) ? cb[id] = react.push(cb) : 0,
-            },
-          },
-        );
-        yield orb;
-      },
-    },
-  })
-);
+export const bindProperties = (orb, obj, ...props) =>
+  orb.onchange((value) => props.forEach((prop) => obj[prop] = value));
+
+const queue$ = [];
+
+export const queue = (cb) =>
+  isFunction(cb)
+    ? function () {
+      const defer = () => cb.apply(this, arguments);
+      queue$.push(defer);
+      return defer;
+    }
+    : cb;
+
+export const flush = () => {
+  while (queue$.length) queue$.pop()();
+};
