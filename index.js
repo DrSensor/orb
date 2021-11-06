d1817cc3857b5ccb9bac80e42ba2565b5d31b0c1
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-06 16:06:02 +0700

    Support multiple listener

--- index.js
+++ index.js
@@ -6,18 +6,30 @@ const isFunction = ($) => typeof $ == "function";
     [Symbol.iterator]: {
       value: function* () {
         let self = this, onchange = noop;
+        const react = [];
         const orb = defineProperties(
           (...$) => {
             if ($.length) {
               onchange(self = $[0]);
+              react.forEach((effect) => effect(self));
             }
             return self;
           },
           {
             reset: { value: () => orb(this) },
             value: { set: orb, get: orb },
+            unbind: {
+              value: ($) => {
+                const { length } = react;
+                isFunction($)
+                  ? react.splice(react.findIndex((cb) => cb[id] == $[id]), 1)
+                  : react.splice($ > 0 ? $ - 1 : length, 1);
+                return length;
+              },
+            },
             onchange: {
               set: (cb) => onchange = isFunction(cb) ? cb : noop,
+              get: () => (cb) => isFunction(cb) ? cb[id] = react.push(cb) : 0,
             },
           },
         );
