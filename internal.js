ef9726d0026ab8c27adb7455c4916c0f6cad0e2f
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-07 12:04:09 +0700

    Fix cannot access 'orb' before initialization

--- internal.js
+++ internal.js
@@ -5,30 +5,28 @@ const { defineProperties } = Object, { iterator } = Symbol;
     [iterator]: {
       value: function* () {
         let self = this;
-        const orb = defineProperties(
-          (...$) => {
-            const [$1] = $;
-            if (typeof $1 == "function") { // cascading transformed orb
-              return cascade((orb$) => (value) => orb$($1(value)));
-            } else { // set/get orb
-              if ($.length) {
-                const finalize = orb.onchange?.(self = $1), after = [];
-                for (const effect of orb.effect) after.push(effect(self));
-                while (after.length) after.shift()?.();
-                finalize?.();
-              }
-              return self;
+        const orb = (...$) => {
+          const [$1] = $;
+          if (typeof $1 == "function") { // cascading transformed orb
+            return cascade((orb$) => (value) => orb$($1(value)));
+          } else { // set/get orb
+            if ($.length) {
+              const finalize = orb.onchange?.(self = $1), after = [];
+              for (const effect of orb.effect) after.push(effect(self));
+              while (after.length) after.shift()?.();
+              finalize?.();
             }
+            return self;
+          }
+        };
+        defineProperties(orb, {
+          effect: { value: new Set() },
+          initial: { value: this },
+          value: { set: orb, get: orb },
+          [iterator]: function* () { // cascading orb
+            yield cascade((orb$) => orb$);
           },
-          {
-            effect: { value: new Set() },
-            initial: { value: this },
-            value: { set: orb, get: orb },
-            [iterator]: function* () { // cascading orb
-              yield cascade((orb$) => orb$);
-            },
-          },
-        );
+        });
         const cascade = (mkEffect) => {
           const [orb$] = $[iterator].apply(self); // BUG: seems it only able to cascade one level 🤔
           orb.effect.add(mkEffect(orb$));
