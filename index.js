2f1d547695837c60d363c068142e1ac72ddbe69e
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-07 11:38:58 +0700

    Add utils for accessing orb ancestor/descendant

--- index.js
+++ index.js
@@ -23,6 +23,27 @@ export const binds = (orbs, dict) =>
 
 export const reset = (orb) => orb(orb.initial);
 
+export const root = (orb) => Array.from(ancestor(orb)).at(-1);
+
+export function* ancestor(orb) {
+  let parent = orb.inherit;
+  yield parent;
+  while (parent = parent.inherit) yield parent;
+}
+
+export function* children(orb) {
+  for (const effect of orb.effect) if (effect.inherit === orb) yield effect;
+}
+
+export function* descendant(orb) {
+  // yield* descendant(yield* children); // TODO: check if recursive Generator `yield* self(yield* other)` works
+  // TODO: replace 👇 if 👆 works!
+  for (const child of children(orb)) {
+    yield child;
+    yield* descendant(child);
+  }
+}
+
 export const insync = (orb) => orb.inherit?.effect.has(orb),
   sync = (orb) => orb.inherit?.effect.add(orb),
   unlink = (orb) => orb.inherit?.effect.delete(orb),
