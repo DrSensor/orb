c234f574610985ea34916a00399837f8cc03602b
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-08 07:27:22 +0700

    Expose factory method for creating orb

---
+++ factory.js
@@ -0,0 +1,40 @@
+const { defineProperties } = Object, { iterator } = Symbol;
+const isFunction = ($) => typeof $ == "function", create = {};
+
+// keep the function.name anonymous
+create.orb = (self) => {
+  const orb = (...$) => {
+    const [$1] = $;
+    if (isFunction($1)) { // cascading transformed orb
+      return cascade((orb$) => (value) => orb$($1(value)));
+    } else { // set/get orb
+      if ($.length) {
+        self = $1;
+        const finalize = orb.onchange?.(self), after = [];
+        for (const effect of orb.effect) after.push(effect(self));
+        for (const finalize of after) if (isFunction(finalize)) finalize();
+        finalize?.();
+      }
+      return self;
+    }
+  };
+
+  const cascade = (mkEffect) => {
+    const orb$ = create.orb(self);
+    orb.effect.add(mkEffect(orb$));
+    return defineProperties(orb$, { inherit: { value: orb } });
+  };
+
+  return defineProperties(orb, {
+    effect: { value: new Set() },
+    initial: { value: self },
+    value: { set: orb, get: orb },
+    [iterator]: { // cascading orb
+      *value() {
+        yield cascade((orb$) => orb$);
+      },
+    },
+  });
+};
+
+export default create.orb;
--- internal.js
+++ internal.js
@@ -1,43 +1,9 @@
-const { defineProperties } = Object, { iterator } = Symbol;
-const isFunction = ($) => typeof $ == "function";
+import orb from "./factory.js";
 
 [Boolean, Number, String, BigInt].forEach(({ prototype: $ }) =>
-  defineProperties($, {
-    [iterator]: {
-      *value() {
-        let self = this;
-        const orb = (...$) => {
-          const [$1] = $;
-          if (isFunction($1)) { // cascading transformed orb
-            return cascade((orb$) => (value) => orb$($1(value)));
-          } else { // set/get orb
-            if ($.length) {
-              self = $1;
-              const finalize = orb.onchange?.(self), after = [];
-              for (const effect of orb.effect) after.push(effect(self));
-              for (const finalize of after) isFunction(finalize) && finalize();
-              finalize?.();
-            }
-            return self;
-          }
-        };
-        defineProperties(orb, {
-          effect: { value: new Set() },
-          initial: { value: this },
-          value: { set: orb, get: orb },
-          [iterator]: { // cascading orb
-            *value() {
-              yield cascade((orb$) => orb$);
-            },
-          },
-        });
-        const cascade = (mkEffect) => {
-          const [orb$] = $[iterator].apply(self);
-          orb.effect.add(mkEffect(orb$));
-          return defineProperties(orb$, { inherit: { value: orb } });
-        };
-        yield orb;
-      },
+  Object.defineProperty($, Symbol.iterator, {
+    *value() {
+      yield orb(this);
     },
   })
 );
