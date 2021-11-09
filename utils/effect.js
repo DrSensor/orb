bae600867949655f32d08df05b2ebf083ce73393
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-09 08:57:07 +0700

    Optimize effect queue when timeout not defined
    [Why?]
    (.unshift -> .pop) is slower than (.push -> .reverse -> .pop)
    because .unshift *most of the time* has to re-index the whole array
    while .push simply just set the last element in the array. Note that
    .reverse is an in-place algorithm which has O(1) space complexity.

--- utils/effect.js
+++ utils/effect.js
@@ -2,18 +2,23 @@ export const useEffect = (effect, orbs) =>
   orbs.forEach((orb) => orb.onchange = effect);
 
 const queue$ = [];
+let deadline;
 
-export const queue = (effect) =>
-  function () {
+export const queue = (effect) => {
+  const enqueue = deadline ? queue$.unshift : queue$.push;
+  return function () {
     const defer = () => effect.apply(this, arguments);
-    queue$.unshift(defer);
+    enqueue(defer);
     return defer;
   };
+};
 
-export const flush = (timeout = Infinity) => {
+export const flush = (timeout) => {
+  deadline = timeout;
   const startTime = performance.now();
   while (queue$.length) {
+    if (deadline) queue$.reverse();
     queue$.pop()();
-    if (performance.now() - startTime > timeout) break;
+    if (deadline && performance.now() - startTime > timeout) break;
   }
 };
