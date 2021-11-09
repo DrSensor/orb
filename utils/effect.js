d4e52ac5be06839a891f9b8efb5a2deeb0cfd14f
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-09 09:28:18 +0700

    Move check at construct time instead call time

--- utils/effect.js
+++ utils/effect.js
@@ -3,25 +3,27 @@ export const useEffect = (effect, orbs) =>
 
 export class QueueEffect {
   constructor(deadline, size) {
-    const queue$ = Array(size ?? 0);
-
+    const queue$ = Array(size ?? 0),
+      enqueue = deadline ? queue$.unshift : queue$.push,
+      dequeue = deadline
+        ? (startTime) => {
+          queue$.pop()();
+          return performance.now() - startTime < deadline; // continue flush-ing if still meet the deadline
+        }
+        : () => {
+          queue$.reverse();
+          queue$.pop()();
+        };
     return [
-      (effect) => {
-        const enqueue = deadline ? queue$.unshift : queue$.push;
-        return function () {
+      /*queue*/ (effect) =>
+        function () {
           const defer = () => effect.apply(this, arguments);
           enqueue(defer);
           return defer;
-        };
-      },
-
-      () => {
+        },
+      /*flush*/ () => {
         const startTime = performance.now();
-        while (queue$.length) {
-          if (deadline) queue$.reverse();
-          queue$.pop()();
-          if (performance.now() - startTime > deadline) break;
-        }
+        while (queue$.length) if (!dequeue(startTime)) break;
       },
     ];
   }
