32f064842aa01cf0cd0c2e69e2c5542fba05114b
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-14 13:20:58 +0700

    Refactor class QueueEffect

--- utils/effect.js
+++ utils/effect.js
@@ -33,9 +33,18 @@ export const replaceEffect = (prevEffect, newEffect, ...orbs) =>
 
 export class QueueEffect {
   constructor(deadline, size) {
-    const effectPool = Array(size ?? 0),
-      enqueue = deadline ? effectPool.unshift : effectPool.push,
-      dequeue = deadline
+    const effectPool = Array(size ?? 0);
+    return (function* () {
+      const enqueue = deadline ? effectPool.unshift : effectPool.push;
+
+      /** queue */ yield (effect) =>
+        function () {
+          const defer = () => effect.apply(this, arguments);
+          enqueue(defer);
+          return defer;
+        };
+
+      const dequeue = deadline
         ? (startTime) => {
           effectPool.pop()();
           return performance.now() - startTime < deadline; // continue flush-ing if still meet the deadline
@@ -44,19 +53,13 @@ export class QueueEffect {
           effectPool.reverse();
           effectPool.pop()();
         };
-    return [
-      /*queue*/ (effect) =>
-        function () {
-          const defer = () => effect.apply(this, arguments);
-          enqueue(defer);
-          return defer;
-        },
-      /*flush*/ () => {
+
+      /** flush */ yield () => {
         const startTime = performance.now();
         while (effectPool.length) if (!dequeue(startTime)) break;
-      },
-      effectPool, // in case someone want to class extends QueueEffect
-    ];
+        return effectPool; // in case someone want to class extends QueueEffect or check the Queue size/length
+      };
+    })();
   }
 }
 
