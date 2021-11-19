05443e68796ee8f87cf62763ef2d9c48278daf95
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-19 19:58:14 +0700

    Replace html attr binds implementation with XPath

--- runtime/web/html-progressive.mjs
+++ runtime/web/html-progressive.mjs
@@ -2,19 +2,22 @@ import "../index.js";
 
 Object.defineProperty(Element.prototype, "binds", {
   value(obj) {
-    const bindAttrsIn = (element) => {
-      for (const { name, value } of element.attributes) {
-        if (name.endsWith(":")) { // TODO: replace all with XPath query
-          obj[value]?.effect.add((value) =>
-            element.setAttribute(name.slice(0, name.length - 1), value)
-          );
-        }
-      }
-    };
     const isScript = this instanceof HTMLScriptElement ||
       this instanceof SVGScriptElement;
-    const parent = isScript ? this.parentElement : this;
-    bindAttrsIn(parent); // TODO: schedule 👈 & 👇 using https://developer.mozilla.org/en-US/docs/Web/API/Background_Tasks_API#running_tasks
-    for (const child of parent.getElementsByTagName("*")) bindAttrsIn(child);
+
+    const endsWith = (haystack, needle) =>
+      `substring(${haystack},string-length(${haystack})-string-length(${needle})+1)=${needle}`;
+
+    const result = document.evaluate(
+      `.//@*[${endsWith("name()", "':'")}]`,
+      isScript ? this.parentElement : this,
+      null,
+      6,
+    ); // TODO: schedule 👈 & 👇 using https://developer.mozilla.org/en-US/docs/Web/API/Background_Tasks_API#running_tasks
+
+    for (let i = 0; i < result.snapshotLength; i++) {
+      const attr = result.snapshotItem(i);
+      obj[attr.value]?.effect.add((value) => attr.nodeValue = value);
+    }
   },
 });
