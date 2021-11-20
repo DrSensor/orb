733e4c8391ef740c2781e8fe5b5a3c454e96e0d0
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-20 08:16:48 +0700

    Fix html walrus attribute not working
    [Cause] It only binds walrus attribute instead of real attribute
    
    [Fix] Create and binds real attribute then remove walrus attribute

--- runtime/web/html-progressive.mjs
+++ runtime/web/html-progressive.mjs
@@ -12,12 +12,14 @@ Object.defineProperty(Element.prototype, "binds", {
       `.//@*[${endsWith("name()", "':'")}]`,
       isScript ? this.parentElement : this,
       null,
-      6,
+      6, // UNORDERED_NODE_SNAPSHOT_TYPE
     ); // TODO: schedule 👈 & 👇 using https://developer.mozilla.org/en-US/docs/Web/API/Background_Tasks_API#running_tasks
 
     for (let i = 0; i < result.snapshotLength; i++) {
-      const attr = result.snapshotItem(i);
-      obj[attr.value]?.effect.add((value) => attr.nodeValue = value);
+      const { value, name, ownerElement } = result.snapshotItem(i);
+      const attr = document.createAttribute(name.slice(0, name.length - 1));
+      obj[value]?.effect.add((value) => attr.nodeValue = value);
+      ownerElement.removeAttribute(name);
     }
   },
 });
