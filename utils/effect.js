7fa1bfc22f317358d4cf48f6387c9f771855f042
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-27 12:35:51 +0700

    Add additional utils related to orb effect

--- utils/effect.js
+++ utils/effect.js
@@ -1,10 +1,24 @@
 export const setEffect = (effect, orbs) =>
-  orbs.forEach((orb) => orb.effect = effect);
+  orbs.forEach((orb) => orb.effect = effect) || effect;
 
-export const unsetEffect = (orbs, effect) =>
+export const unsetEffect = (effect, orbs) =>
   orbs.forEach((orb) =>
     (!effect || effect == orb.effect) && (orb.effect = undefined)
-  );
+  ) || effect;
+
+const each = (orbs, o, e, effect) => orbs[o]((orb) => orb.effect[e](effect));
+
+export const addEffect = (effect, orbs) =>
+  each(orbs, "forEach", "add", effect) || effect;
+
+export const deleteEffect = (effect, orbs) =>
+  each(orbs, "forEach", "delete", effect) || effect;
+
+export const someHasEffect = (effect, orbs) =>
+  each(orbs, "some", "has", effect);
+
+export const everyHasEffect = (effect, orbs) =>
+  each(orbs, "every", "has", effect);
 
 /** **WARNING** very slow operation */
 export const replaceEffect = (prevEffect, newEffect, ...orbs) =>
