e0f50cec721d43b253b062cf9e2381e81c8a4bc2
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-11-29 02:41:36 +0700

    Fix memory leak in jsx-runtime baseline

--- runtime/jsx-runtime.js
+++ runtime/jsx-runtime.js
@@ -3,33 +3,34 @@
 const TOPLEVEL = 2, AUTOMATIC = 1, CLASSIC = 0;
 export const pipeline = (mode, ...jsxFactories) =>
   (element, props, ...args) => {
-    const runtime = {}, after = [];
-    const children = !mode ? args : (
-      (children) => children[Symbol.iterator] ? children : [children]
-    )(props?.children);
+    const runtime = {}, call = {}, after = [];
+    if (mode) {
+      var { children, ...props } = props;
+      children = children[Symbol.iterator] ? children : [children];
+    } else children = args;
 
-    args = [props].concat(children, args);
+    args = [props].concat(children, runtime, args);
     for (let create of jsxFactories) {
-      element = create.apply(runtime, [element].concat(args)) ?? element;
-      if (typeof (create = create.effect?.()) == "function") after.push(create);
+      element = create.apply(call, [element].concat(args)) ?? element;
+      if (typeof (create = call.effect?.()) == "function") after.push(create);
     }
     for (const effect of after) effect?.();
     return element;
-  }; // BUG: 👈 effect Map should be evicted here
+  }; // 👈 effect Map should be evicted here
 
 const O = Object;
-export function create(element, props, children, ...extras) {
+export function create(element, props, children, runtime, ...extras) {
   const [key, ...DEV] = extras, [isStaticChildren, source, self] = DEV;
   if (typeof element == "function") {
     const effect = new Map().set(undefined, new Set()); // PERF: beware of dangling object (unused memory)
-    create.effect = () =>
+    this.effect = () =>
       () => {
         for (const [, effects] of effect) { // apply all effect Map at the end of pipeline
           for (const effect of effects) effect();
         }
       }; // 👈 effect Map not yet evicted here
     return element.call(
-      O.defineProperties(this, {
+      O.defineProperties(runtime, {
         effect: {
           get: () =>
             (orbs) => { // WARNING: this is inefficient compared to ECS Archetype approach
