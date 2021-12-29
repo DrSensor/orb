e4c9157565b66ca346ee402745ebec1fb09f7df8
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-12-29 10:24:10 +0700

    Add JSX pipeline that utilize plain structure-tree

---
+++ runtime/web/pipeline/structure-tree.js
@@ -0,0 +1,34 @@
+import { bindElementNS, hasNamespace } from "./_internal.js";
+import * as create from "./create-element.js";
+
+export const build = (element, props, children) =>
+  !hasNamespace(element) && [
+    element,
+    children,
+    ...props.reduce((partition, prop) => (
+      partition[+(typeof prop == "function")].push(prop), partition
+    ), [[], []]),
+  ];
+
+const attach = (element, children) => {
+  for (let child of children) {
+    if (Array.isArray(child)) {
+      const [tag, children, staticProps, dynamicProps] = child;
+      attach(
+        child = bindElementNS(
+          element.namespaceURI,
+          tag,
+          staticProps.concat(dynamicProps),
+        ),
+        children,
+      );
+    }
+    element.append(child);
+  }
+};
+
+export const createOnNamespace = create.ifHasNamespace(attach);
+
+export const createOnElement = create.ifElement(attach);
+
+export const createOnDocumentFragment = create.ifDocumentFragment(attach);
