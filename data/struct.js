205a029c65f7258d044e6f2c2357ecfe4c1198ce
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-01-04 17:41:50 +0700

    Refactor reactive struct
    * Add `set` property descriptor on each primitive value
    
    * Make 2nd arguments as an config option
    
    * Support add/rewrite property descriptor

--- data/struct.js
+++ data/struct.js
@@ -1,16 +1,30 @@
-import Orb from "./reactive.js";
-import { isFunction, isObject, iterator, O } from "./_internal.js";
-const map = ($, to) => O.fromEntries(O.entries($).map(to));
+import Orb, { sequential } from "./reactive.js";
+import {
+  defineProperties,
+  isFunction,
+  isObject,
+  iterator,
+  O,
+} from "./_internal.js";
 
-export default function (struct, depth = 1) {
-  const transform = (level) =>
-    ([key, value]) => [ // TODO: refactor into property descriptor { get: () => orb, set: orb.set }
-      key,
-      isObject(value) && level < depth
-        ? map(value, transform(++level))
-        : value[iterator] || isFunction(value)
-        ? value
-        : Orb.call(this, value),
-    ];
-  return map(struct, transform(1));
+export default function (struct, { depth = 1, mode = sequential, ...p } = {}) {
+  if (!mode[iterator]) mode = [mode, mode];
+  const transform = (struct, result = final, level = 1) => {
+      const orb = {}, desc = {};
+      for (const key in struct) {
+        const value = struct[key],
+          get = () => orb[key] ??= Orb.call(this, value, ...mode),
+          isObject$ = isObject(value),
+          isIterable$ = isObject$ && iterator in value;
+
+        !isIterable$ && level < depth
+          ? transform(value, result[key] = {}, ++level)
+          : isObject$ || isIterable$ || isFunction(value)
+          ? result[key] = value
+          : desc[key] = { get, set: (value) => get().set(value), ...p };
+      }
+      defineProperties(result, desc);
+    },
+    final = {};
+  return transform(struct), final;
 }
