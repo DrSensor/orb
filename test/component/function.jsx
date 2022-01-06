3e280b6251a4c2342569b354f0a98f6ee3b18265
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-01-06 07:40:19 +0700

    Update draft specs for <FunctionComponent>

--- test/component/function.jsx
+++ test/component/function.jsx
@@ -6,6 +6,27 @@ export default (title = "<FunctionComponent>") =>
     it("should pass children in `arguments[1..]`");
     it("can be created without props and children");
 
+    describe("when created", () => {
+      it("should throw error if return is not JSX.Element");
+      it("should produce the same JSX.Element if return is <non-component>");
+      describe("when return is <FunctionComponent>", () => {
+        describe("that return <non-component>", () => {
+          describe("should return instanceof Element", () => {
+            it("if with namespace");
+            it("if with instanceof Element");
+          });
+          it("should return function or tree if without namespace");
+        });
+        describe("that return <FunctionComponent>", () => {});
+      });
+
+      describe("effect", () => {
+        context("in function Component", () => {});
+        context("in function hook", () => {});
+      });
+    });
+
+    // TODO: move to 👆 maybe 🤔 (or nay since 👆 is for complex stuff like passing arguments, context, etc)
     describe("when used as child", () => {
       context("in <non-component>", () => {
         describe("with namespace", () => {
@@ -36,5 +57,22 @@ export default (title = "<FunctionComponent>") =>
           it("shouldn't yet execute `this(hook)`");
         });
       });
+
+      // WARNING: seems redundant, maybe just check the return value 🤔
+      // Actually, there is a lot of combination since it's nested. So maybe use property-based-test 🤔
+      // context("in <FunctionComponent> that return <non-component>", () => {
+      //   describe("with namespace", () => {
+      //     it("should execute `this.effect`");
+      //     it("should execute `this(hook)`");
+      //   });
+      //   describe("with instanceof Element", () => {
+      //     it("should execute `this.effect`");
+      //     it("should execute `this(hook)`");
+      //   });
+      //   describe("without namespace", () => {
+      //     it("shouldn't yet execute `this.effect`");
+      //     it("shouldn't yet execute `this(hook)`");
+      //   });
+      // });
     });
   });
