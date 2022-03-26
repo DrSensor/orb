1d1a8ca403d2daad37bf7356724d3417c7fc84ba
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-03-27 00:54:23 +0700

    Block `await channel` until channel send something

--- data/convention/channel.js
+++ data/convention/channel.js
@@ -1,18 +1,20 @@
-const O = Object, A = Array, go = queueMicrotask,
+const O = Object, A = Array,
   { seal, defineProperties } = O,
   push = ($, value) => $.push(value), pop = ($) => $.pop($),
-  shift = ($) => $.shift(), unshift = ($, value) => $.unshift(value);
+  shift = ($) => $.shift(), unshift = ($, value) => $.unshift(value),
+  microtask = queueMicrotask;//, macrotask = setTimeout;
 
 export default (buffer, label) => {
   let isSealed = !!buffer, isFastSend = buffer > 0, index = 0,
     set = (value) => isSealed ? buffer[index++] = value
       : (isFastSend ? push : unshift)(buffer, value);
 
-  buffer = buffer ? seal([...A(buffer)]) : []; // WARNING: error early on send instead of recv since it's sealed
+  buffer = buffer ? seal([...A(buffer)]) : []; // WARNING: error early on send instead error on recv since it's sealed
 
   return defineProperties({
-    then: (resolve) => go(() => resolve(
-      isSealed ? buffer[index--] : (isFastSend ? shift : pop)()
-    )), set
+    then: (resolve) => microtask(() => {
+      while (index == 0); // block until channel receive value via `set`
+      resolve(isSealed ? buffer[--index] : (isFastSend ? shift : pop)());
+    }), set
   }, { let: { set } });
 };
