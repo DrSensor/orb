85c8c7595bfd5f3210f87ead813b6429dca9b016
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-03-26 10:59:13 +0700

    Draft data channel implementation for message passing

--- data/convention/channel.js
+++ data/convention/channel.js
@@ -1,1 +1,18 @@
-export default (buffer = 1, label) => new Promise((resolve) => {});
+const O = Object, A = Array, go = queueMicrotask,
+  { seal, defineProperties } = O,
+  push = ($, value) => $.push(value), pop = ($) => $.pop($),
+  shift = ($) => $.shift(), unshift = ($, value) => $.unshift(value);
+
+export default (buffer, label) => {
+  let isSealed = !!buffer, isFastSend = buffer > 0, index = 0,
+    set = (value) => isSealed ? buffer[index++] = value
+      : (isFastSend ? push : unshift)(buffer, value);
+
+  buffer = buffer ? seal([...A(buffer)]) : []; // WARNING: error early on send instead of recv since it's sealed
+
+  return defineProperties({
+    then: (resolve) => go(() => resolve(
+      isSealed ? buffer[index--] : (isFastSend ? shift : pop)()
+    )), set
+  }, { let: { set } });
+};
