c968392ee09222d5781cec87530add8a924e3867
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-04-04 05:21:11 +0700

    Make await channel can be throttled via .then(ms)

--- data/channel.js
+++ data/channel.js
@@ -1,6 +1,8 @@
 const O = Object, A = Array, M = Math, S = Symbol,
   { is, seal, defineProperties } = O, { sign, abs } = M, { asyncIterator } = S,
+  type = ($) => typeof $, isFunction = ($) => type($) == "function", identity = ($) => $,
   isNonZero = ($) => $ && isFinite($), isNegative = ($) => sign($) == -1 || is($, -0),
+  sleep = (ms) => new Promise(resolve => setTimeout(resolve), ms),
   push = ($, value) => $.push(value), pop = ($) => $.pop($), shift = ($) => $.shift(),
   length = "length";//, macrotask = setTimeout;
 
@@ -15,8 +17,10 @@ export default (buffer, label) => {
   return defineProperties({
     async *[asyncIterator]() { while (occupied()) yield await this; },
     async then(resolve) {
-      while (!occupied()) await 0; // block until channel receive value via `set`
-      return resolve(isSealed ? buffer[isFIFO ? --index : buffer[length] - index--] : (isFIFO ? shift : pop)());
+      while (!occupied()) await (isFunction(resolve) || sleep(resolve)); // block until channel receive value via `set`
+      return (isFunction(resolve) ? resolve : identity)(
+        isSealed ? buffer[isFIFO ? --index : buffer[length] - index--] : (isFIFO ? shift : pop)()
+      );
     }, set
   }, { let: { set }, [length]: { get: occupied } });
 };
