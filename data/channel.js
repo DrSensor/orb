fddcbeba69d174624306c4bd9bdffa8892945aed
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-04-04 06:45:02 +0700

    Add non-async iterator method to peek channel buffer

--- data/channel.js
+++ data/channel.js
@@ -1,10 +1,12 @@
 const O = Object, A = Array, M = Math, S = Symbol,
-  { is, seal, defineProperties } = O, { sign, abs } = M, { asyncIterator } = S,
+  { is, seal, defineProperties } = O, { sign, abs } = M, { asyncIterator, iterator } = S,
   type = ($) => typeof $, isFunction = ($) => type($) == "function", identity = ($) => $,
   isNonZero = ($) => $ && isFinite($), isNegative = ($) => sign($) == -1 || is($, -0),
-  sleep = (ms) => new Promise(resolve => setTimeout(resolve), ms),
+  sleep = (ms) => new Promise(resolve => setTimeout(resolve), ms), // macrotask = setTimeout;
   push = ($, value) => $.push(value), pop = ($) => $.pop($), shift = ($) => $.shift(),
-  length = "length";//, macrotask = setTimeout;
+  slice = ($, start, end) => $.slice(start, end),
+  first = ($, n) => slice($, 0, n), last = ($, n) => slice($, -n),
+  length = "length";
 
 export default (size, label) => {
   const isSealed = isNonZero(size), isFIFO = !isNegative(size),
@@ -14,6 +16,7 @@ export default (size, label) => {
 
   size = 0;
   return defineProperties({
+    *[iterator]() { yield* (isSealed ? (isFIFO ? first : last)(buffer, size) : buffer); },
     async *[asyncIterator]() { while (occupied()) yield await this; },
     async then(resolve) {
       while (!occupied()) await (isFunction(resolve) || sleep(resolve)); // block until channel receive value via `set`
