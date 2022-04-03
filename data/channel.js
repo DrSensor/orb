85fa6a7c0b55a6523860b5d51adca777ad677434
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-04-03 10:10:35 +0700

    Add stack/LIFO channel mode
    if buffer size is:
    * negative (i.e -0, -9, -Infinity) -> stack/LIFO mode
    * positive (i.e 0, 2, Infinity) -> queue/FIFO mode

--- data/channel.js
+++ data/channel.js
@@ -1,20 +1,22 @@
-const O = Object, A = Array,
-  { is, seal, defineProperties } = O,
+const O = Object, A = Array, M = Math,
+  { is, seal, defineProperties } = O, { sign, abs } = M,
+  isNonZero = ($) => $ && isFinite($), isNegative = ($) => sign($) == -1 || is($, -0),
   push = ($, value) => $.push(value), pop = ($) => $.pop($),
   shift = ($) => $.shift(), unshift = ($, value) => $.unshift(value),
-  microtask = queueMicrotask;//, macrotask = setTimeout;
+  length = "length", microtask = queueMicrotask;//, macrotask = setTimeout;
 
 export default (buffer, label) => {
-  let isSealed = !!buffer, isFastSend = !is(buffer, -0), index = 0,
-    set = (value) => isSealed ? buffer[index++] = value
-      : (isFastSend ? push : unshift)(buffer, value);
+  let isSealed = isNonZero(buffer), index = isSealed - 1,
+    isFIFO = !isNegative(buffer),
+    set = (value) => isSealed ?
+      buffer[index++] = value : push(buffer, value);
 
-  buffer = buffer ? seal([...A(buffer)]) : []; // WARNING: error early on send instead error on recv since it's sealed
+  buffer = isSealed ? seal([...A(abs(buffer))]) : []; // WARNING: error early on send instead error on recv since it's sealed
 
   return defineProperties({
     then: (resolve) => microtask(() => {
-      while (index == 0); // block until channel receive value via `set`
-      resolve(isSealed ? buffer[--index] : (isFastSend ? shift : pop)());
+      while (!index || !buffer[length]); // block until channel receive value via `set`
+      resolve(isSealed ? buffer[isFIFO ? --index : buffer[length] - index--] : (isFIFO ? shift : pop)());
     }), set
   }, { let: { set } });
 };
