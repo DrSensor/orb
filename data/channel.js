4e64f725f69100beba397dea26ade72b11c1214b
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-04-04 05:16:21 +0700

    Make channel .then() chainable

--- data/channel.js
+++ data/channel.js
@@ -1,9 +1,8 @@
-const O = Object, A = Array, M = Math,
-  { is, seal, defineProperties } = O, { sign, abs } = M,
+const O = Object, A = Array, M = Math, S = Symbol,
+  { is, seal, defineProperties } = O, { sign, abs } = M, { asyncIterator } = S,
   isNonZero = ($) => $ && isFinite($), isNegative = ($) => sign($) == -1 || is($, -0),
-  push = ($, value) => $.push(value), pop = ($) => $.pop($),
-  shift = ($) => $.shift(), unshift = ($, value) => $.unshift(value),
-  length = "length", microtask = queueMicrotask;//, macrotask = setTimeout;
+  push = ($, value) => $.push(value), pop = ($) => $.pop($), shift = ($) => $.shift(),
+  length = "length";//, macrotask = setTimeout;
 
 export default (buffer, label) => {
   const isSealed = isNonZero(buffer), isFIFO = !isNegative(buffer),
@@ -14,12 +13,10 @@ export default (buffer, label) => {
   let index = 0;
 
   return defineProperties({
-    async *[Symbol.asyncIterator]() { while (occupied()) yield await this; },
-    then(resolve) {
-      microtask(() => {
-        while (!occupied()); // block until channel receive value via `set`
-        resolve(isSealed ? buffer[isFIFO ? --index : buffer[length] - index--] : (isFIFO ? shift : pop)());
-      });
+    async *[asyncIterator]() { while (occupied()) yield await this; },
+    async then(resolve) {
+      while (!occupied()) await 0; // block until channel receive value via `set`
+      return resolve(isSealed ? buffer[isFIFO ? --index : buffer[length] - index--] : (isFIFO ? shift : pop)());
     }, set
   }, { let: { set }, [length]: { get: occupied } });
 };
